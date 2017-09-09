70b4c88631540291f35ae8ff8a1bcdec966223e7
lizujia <lizujia0616@163.com>
2017-09-09 14:12:48 +0800

    更新示例

--- springmvc003/src/com/lizj/controller_04/BookController04.java
+++ springmvc003/src/com/lizj/controller_04/BookController04.java
@@ -1,9 +1,10 @@
-package com.lizj.controller_03;
+package com.lizj.controller_04;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +13,8 @@ import org.springframework.web.servlet.ModelAndView;
 import com.lizj.bean.Book;
 
 @Controller
-@RequestMapping("/book")
-public class BookController {
+@RequestMapping("/book04")
+public class BookController04 {
 
     // 模拟数据库存储
     private static List<Book> books;
@@ -34,7 +35,7 @@ public class BookController {
     public ModelAndView getBooks() {
         ModelAndView mv = new ModelAndView();
         mv.addObject("books", books);
-        mv.setViewName("book/books");
+        mv.setViewName("book/books04");
         return mv;
     }
     
@@ -59,7 +60,22 @@ public class BookController {
         mv.addObject("books", books);
         // 重定向,冒号后面不要有空格
         // 默认是请求转发(forward), 可以自己试一下效果
-        mv.setViewName("redirect:/book/books");
+        mv.setViewName("redirect:/book/books04");
+        return mv;
+    }
+    
+    // //book/{bookId}是URL模板, 模板变量{bookId}通过注解@PathVarialbe绑定到方法参数上, 即bookId被赋值
+    @RequestMapping(value="/book/{bookId}")
+    public ModelAndView get(@PathVariable int bookId) {
+        ModelAndView mv = new ModelAndView();
+        for(int i=0; i<books.size(); i++) {
+            Book book = books.get(i);
+            if(book.getId() == bookId) {
+                mv.addObject("book", book);
+                break ;
+            }
+        }
+        mv.setViewName("book/one") ;
         return mv;
     }
     
---
+++ springmvc003/web/WEB-INF/jsp/book/books04.jsp
@@ -0,0 +1,47 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>  
+<%
+    String path = request.getContextPath();
+    String basePath = request.getScheme() + "://"
+            + request.getServerName() + ":" + request.getServerPort()
+            + path + "/";
+%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<base href="<%=basePath%>">
+
+<title>books04</title>
+
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+<meta http-equiv="description" content="This is my page">
+<!--
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	-->
+
+</hea>
+
+<body>
+	<a href="book/add">添加</a> 
+	<table>
+		<tr>
+			<td>序号</td>
+			<td>书名</td>
+			<td>作者</td>
+			<td>操作</td>
+		</tr>
+		<c:forEach items="${books}" varStatus="i" var="item" >
+		<tr>
+			<td>${item.id }</td>
+			<td>${item.bookName }</td>
+			<td>${item.author }</td>
+			<td><a href="book04/book/${item.id }">详情</a></td>
+		</tr>
+		</c:forEach>
+	</table>
+</body>
+</html>
